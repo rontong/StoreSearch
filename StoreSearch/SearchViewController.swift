59b3915139d634339f1ba0d904f7dcfc48bcb520
Ron Tong <ronald_tong@hotmail.com>
2018-08-21 08:26:05 +1000

    Add Search Bar Delegate, TableView DataSource, TableView Delegate

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchResults = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Content inset attribute. Tell tableView to add 64-pt margin at the top (20pt status and 44pt search bar)
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,53 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+extension SearchViewController: UISearchBarDelegate {
+   
+    // Create an array and add searchBar.text to the array
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        searchBar.resignFirstResponder()
+        
+        searchResults = [String]()
+        
+        // %d is a placeholder for integer numbers. %f is for numbers with decimal point. %@is is for objects such as strings.
+        
+        for i in 0...2 {
+            searchResults.append(String(format: "Fake Result %d for '%@'", i, searchBar.text!))
+        }
+        tableView.reloadData()
+    }
+    
+    // Attach search bar to top of the screen
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "SearchResultCell"
+        
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        
+        cell.textLabel!.text = searchResults[indexPath.row]
+        return cell
+    }
+    
+}
+
+extension SearchViewController: UITableViewDelegate {
+    
+}
